c93f06ae29cdac015f0027f322ffa47a752c997d
Spas Kyuchukov <lolotobg@users.noreply.github.com>
2013-06-15 19:30:35 +0000

    Small works on the UI. There is a collision between some of the existing files (commented in the index page).

--- scripts/UI.js
+++ scripts/UI.js
@@ -1,10 +1,12 @@
-var shopCategories = shop.getCategories;
-var tabsIndex = 0;
-for (var categoryI in shopCategories) {
-	var li = document.createElement("li");
-	var a = document.createElement("a");
-	a.href="#tabs-"+tabsIndex;
-	a.innerHTML = shopCategories[categoryI].userCategory.name;
-	li.appendChild(a);
-	$("#category-tabs-list").appendChild(li);
-};
+(function() {
+	var shopCategories = shop.mainShop.getCategories();
+	var tabsIndex = 0;
+	for (var categoryI in shopCategories) {
+		var li = document.createElement("li");
+		var a = document.createElement("a");
+		a.href="#tabs-"+tabsIndex;
+		a.innerHTML = shopCategories[categoryI].userCategory.name;
+		li.appendChild(a);
+		$("#category-tabs-list").appendChild(li);
+	};
+}());
